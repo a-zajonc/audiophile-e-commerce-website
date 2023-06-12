a363dbb6200aabcd5644ad1e79ddbe058a4fda0b
Adriana <97173869+a-zajonc@users.noreply.github.com>
2023-06-12 15:27:10 +0200

    Add About Component

--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -2,3 +2,4 @@ export { NavBar } from "./NavBar/";
 export { Header } from "./Header/";
 export { Categories } from "./Categories/";
 export { ProductComponent } from "./ProductComponent";
+export { About } from "./About/";
