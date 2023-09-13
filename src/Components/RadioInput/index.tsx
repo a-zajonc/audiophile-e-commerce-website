914d2b7159df952aae94fb240a1c905c406463c1
Adriana <97173869+a-zajonc@users.noreply.github.com>
2023-09-13 15:46:31 +0200

    Add React Hook Form and useForm()

--- src/Components/RadioInput/index.tsx
+++ src/Components/RadioInput/index.tsx
@@ -1,23 +1,29 @@
 import styles from "./index.module.scss";
+import type { ComponentPropsWithoutRef } from "react";
+import { forwardRef } from "react";
 
-type Props = {
+type Props = ComponentPropsWithoutRef<"input"> & {
   title: string;
   radioName: string;
   setOption: any;
 };
 
-export const RadioInput = ({ title, radioName, setOption }: Props) => (
-  <label className={styles.label} htmlFor={title}>
-    <input
-      className={styles.input}
-      type="radio"
-      name={radioName}
-      id={title}
-      value={title}
-      onClick={() => {
-        setOption(title);
-      }}
-    />
-    {title}
-  </label>
+export const RadioInput = forwardRef<HTMLInputElement, Props>(
+  ({ title, radioName, setOption, ...inputProps }: Props, ref) => (
+    <label className={styles.label} htmlFor={title}>
+      <input
+        ref={ref}
+        className={styles.input}
+        type="radio"
+        name={radioName}
+        id={title}
+        value={title}
+        onClick={() => {
+          setOption(title);
+        }}
+        {...inputProps}
+      />
+      {title}
+    </label>
+  )
 );
