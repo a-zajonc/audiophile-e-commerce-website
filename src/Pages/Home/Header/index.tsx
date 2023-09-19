238df964d1537646d72fbb216fc2fb76ff68a258
Adriana <97173869+a-zajonc@users.noreply.github.com>
2023-09-19 11:51:30 +0200

    Add Custom Text Component

--- src/Pages/Home/Header/index.tsx
+++ src/Pages/Home/Header/index.tsx
@@ -1,17 +1,26 @@
 import styles from "./index.module.css";
 import { Link } from "react-router-dom";
 import { Button } from "../../../Components/button/button";
+import { Text } from "../../../Components/Text";
 
 export function Header() {
   return (
     <div className={styles.box}>
       <div className={styles.content}>
-        <h3 className={styles.text}>NEW PRODUCT</h3>
-        <h1 className={styles.title}>XX99 Mark II Headphones</h1>
-        <p className={styles.description}>
+        <h3 className={`${styles.text} ${styles.marginBottom}`}>NEW PRODUCT</h3>
+        <h1 className={`${styles.title} ${styles.marginBottom}`}>
+          XX99 Mark II Headphones
+        </h1>
+        <Text
+          color="secondary"
+          fontSize="s"
+          lineHeight="s"
+          opacity={true}
+          style={{ marginBottom: "44px", paddingRight: "30px" }}
+        >
           Experience natural, lifelike audio and exceptional build quality made
           for the passionate music enthusiast.
-        </p>
+        </Text>
         <Link to="/product/xx99-mark-ii-headphones">
           <Button colorScheme="brand">See Product</Button>
         </Link>
